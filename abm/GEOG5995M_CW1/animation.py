47cf561f1ca7f2d9f4b9a93202ef1858c05fe7c0
lena-kilian <43342015+lena-kilian@users.noreply.github.com>
2018-10-29 10:37:21 +0000

    Update animation.py

--- abm/GEOG5995M_CW1/animation.py
+++ abm/GEOG5995M_CW1/animation.py
@@ -5,29 +5,26 @@ import agent_framework
 import csv
 import time
 
-
-n = 200   # number of agents
-neighbourhood = 20
+n = 100   # number of agents
 k = 200 # number of iterations
+neighbourhood = 20
 
-agents = []
 environment = []
-
 with open('in_example.txt', newline='\n') as f:
     reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
     for row in reader:
         environment.append(row)
-        
-# defining the figure
 
-fig = pyplot.figure(figsize=(8, 8))
-
-# creating agents
+agents = []
 while len(agents) < n:
     agents.append(agent_framework.Agents(environment, agents))
+    
+# defining the figure
+fig = pyplot.figure(figsize=(8, 8))
 
-# update function --> makes agents eat, share, move and animates
+# defining the functions
 def update(frame_number):
+    # makes agents eat, share, move and displays the chart
     fig.clear()
     pyplot.imshow(environment)
     pyplot.ylim(0, 100)
@@ -36,14 +33,14 @@ def update(frame_number):
     agents[0].grass_grow() # has to be outside so that it doesn't grow after each inididual agent moved
     for i in range(len(agents)):
         agents[i].eat()
-        agents[i].throw_up()
+        agents[i].regurgitate()
         agents[i].share(neighbourhood)
         agents[i].moveagent()
     for i in range(len(agents)):
-        pyplot.scatter(agents[i].x_position, agents[i].y_position, color=['white', 'black', 'brown'], s=5)
+        pyplot.scatter(agents[i].x_position, agents[i].y_position, color='white', s=10)
 
-# stops the agents once the grass is eaten up in one spot
 def gen_function(b = [0]):
+    # stops the agents once the grass is eaten up in one spot
     start = time.time()
     a = 0
     list = []
@@ -55,14 +52,10 @@ def gen_function(b = [0]):
             yield a
         else:
             end = time.time()
-            print('Timer:', end - start, 'seconds')
-            '''
-            for i in range(len(agents)):
-                if agents[i].environment[agents[i].y_position][agents[i].x_position] <= 0:
-                    print("This is where the grass is gone:", "Y=" + agents[i].y_postion, "X=" + agents[i].x_postion)
-            '''          
+            print('Timer:', end - start, 'seconds')      
             break
 
-                
-#animation = animation.FuncAnimation(fig, update, frames = k, repeat=False) # uses number of iterations for stoping 
 animation = animation.FuncAnimation(fig, update, frames = gen_function(), repeat=False) # uses gen_function for stopping
+'''
+animation = animation.FuncAnimation(fig, update, frames = k, repeat=False) # uses number of iterations (k) for stoping 
+'''
